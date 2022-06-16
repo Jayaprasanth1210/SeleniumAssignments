8ff2859a3083ade48e8bfc197ff7104a9839415a
Jayaprasanth1210 <61543000+Jayaprasanth1210@users.noreply.github.com>
2022-06-16 10:31:15 +0530

    Update PrimeNumber.java

--- week1/day1/assignments/PrimeNumber.java
+++ week1/day1/assignments/PrimeNumber.java
@@ -1,61 +1,44 @@
-package week1.day1.assignments;
+package week1.day1;
 
 
 
 public class PrimeNumber {
 
-	
-
-	/*
-
-	 * Goal: To find whether a number is a Prime number or not
-
-	 * 
-
-	 * input: 13
-
-	 * output: 13 is a Prime Number
-
-	 * 
-
-	 * Shortcuts:
-
-	 * 1) Print : type: syso, followed by: ctrl + space + enter
-
-	 * 2) To create a 'for' loop: type 'for', followed by ctrl + space + down arrow + enter
-
-	 * 3) To create an 'if' condition: type 'if', followed by ctrl + space +down arrow + enter
-
-	 *   
-
-	 * What are my learnings from this code?
-
-	 * 1)
-
-	 * 2)
-
-	 * 3) 
-
-	 * 
-
-	 */
-
-	
 
 	public static void main(String[] args) {
 
 
 
 		// Declare an int Input and assign a value 13
+		int i=13;
 
 		
 
 		// Declare a boolean variable flag as false
+		boolean flag= false;
 
 		
 
 		// Iterate from 2 to half of the input
-
+		
+			for (int j=2;j<=i;j++)
+			{
+				if (j%1==0 && j%j==0)
+				{
+					flag=true;
+				}
+				break;
+			}
+		if (flag == true)
+		{
+			System.out.println (i + " is prime");
+		}
+		else
+		{
+			System.out.println(i+" is not prime");
+		}
+	
+		
 		
 
 			// Divide the input with each for loop variable and check the remainder
@@ -81,3 +64,4 @@ public class PrimeNumber {
    }
 
 }
+
